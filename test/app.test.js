2f6bbb578e69cb96249a1e656bd84bafb25c5eef
Muhammad Qasim <qasim@4iisolutions.com>
2025-03-16 00:28:00 +0500

    Fix test to check for HTML markup in response

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ describe('GET /', () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toContain('Welcome to Camden Health System');
-    expect(response.text).toContain('This application is live by Muhammad Qasim, Sr. DevOps Engineer');
+    expect(response.text).toContain('This application is live by <strong>Muhammad Qasim, Sr. DevOps Engineer</strong>');
   });
 
   // Close the server after all tests are done
